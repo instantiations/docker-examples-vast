a7e8dc1305747e01ad9e332b12b41741d30d09a4
Mariano Martinez Peck <marianopeck@gmail.com>
2019-05-01 15:35:51 -0400

    more cleanup to dependencies resolution, now do not use anymore absolute paths and other improvements

--- source/SeasideTrafficLights/Raspberry/app/seasideTrafficLight.sh
+++ source/SeasideTrafficLights/Raspberry/app/seasideTrafficLight.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# cd /home/pi/SeasideTrafficLight
 cd /opt/app
 
 export LANG=en_US.iso88591
 export VAST_ROOT="/opt/vast92"
-export LD_LIBRARY_PATH="${VAST_ROOT}/bin"
-$LD_LIBRARY_PATH/esnx -no_break -msd -mcd -i./seasideTrafficLight-unix.icx -ini:./seasideTrafficLight-unix.ini
+export LD_LIBRARY_PATH="${VAST_ROOT}/bin:$LD_LIBRARY_PATH"
+$VAST_ROOT/bin/esnx -no_break -msd -mcd -i./seasideTrafficLight-unix.icx -ini:./seasideTrafficLight-unix.ini
 
 cp *.sdf /opt/log
