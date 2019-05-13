65c6597840547a69b7eeba36556358c992cff500
Mariano Martinez Peck <marianopeck@gmail.com>
2019-05-13 16:41:38 -0400

    * Fix /log mappings to the self contained log directory of the example and add it to gitignore. * Add HEALTHCHECK in case the va process is alive but the seaside server is not responding. * Added missing timezone when running within swarm

--- source/SeasideTrafficLights/Raspberry64/app/seasideTrafficLight.sh
+++ source/SeasideTrafficLights/Raspberry64/app/seasideTrafficLight.sh
@@ -7,4 +7,4 @@ export VAST_ROOT="/opt/vast92"
 export LD_LIBRARY_PATH="${VAST_ROOT}/bin:$LD_LIBRARY_PATH"
 $VAST_ROOT/bin/esnx -no_break -msd -mcd -i./seasideTrafficLight-unix64.icx -ini:./seasideTrafficLight-unix64.ini
 
-cp *.sdf /opt/log
+cp *.sdf /opt/log/
--- source/SeasideTrafficLights/Raspberry64/startSwarm.sh
+++ source/SeasideTrafficLights/Raspberry64/startSwarm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 docker stack deploy --compose-file docker-compose.yml seaside-debian-slim
